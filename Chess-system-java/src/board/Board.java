bde3981a1d410a7ba0a5620b2fd3739e942d630e
MarceloDebug <marcelinho.soares300@gmail.com>
2025-06-03 12:30:36 -0300

    Moving pieces

--- Chess-system-java/src/board/Board.java
+++ Chess-system-java/src/board/Board.java
@@ -51,11 +51,25 @@ public class Board {
     public boolean positionExists(Position position){
         return positionExists(position.getRow(), position.getColumn());
     }
+
     public boolean thereIsAPiece(Position position){
         if(!positionExists(position)){
             throw new BoardException("Position not on the board");
         }
        return piece(position) != null;
+    }
+
+    public Piece removePiece(Position position){
+        if(!positionExists(position)){
+            throw new BoardException("Position not on the board");
+        }
+        if(piece(position) == null){
+            return null;
+        }
+        Piece aux = piece(position);
+        aux.position = null;
+        pieces[position.getRow()][position.getColumn()] = null;
+        return aux;
     }
 
 }
